9a2f218c4078cb3ccd0ec6baf6575579e7dfa4e3
unknown <webcatdev@gmail.com>
2022-02-10 18:50:24 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const isimText=document.getElementById("myName");
+const clock=document.getElementById("myClock");
+
+let isim= prompt("İsminiz:")
+
+isimText.textContent=isim;
+
+function render(){
+    const date = new Date();
+    const gunler = [
+      "Pazartesi",
+      "Salı",
+      "Çarşamba",
+      "Perşembe",
+      "Cuma",
+      "Cumartesi",
+      "Pazar",
+    ];
+    const [hour, minutes, seconds, day] = [
+      date.getHours(),
+      date.getMinutes(),
+      date.getSeconds(),
+      gunler[date.getDay() - 1],
+    ];
+    clock.textContent = `${hour}:${minutes}:${seconds}, ${day}`;
+}
+
+function renderDate() {
+    setInterval(()=>{console.log("adsds");
+    render()
+
+    }, 1000)
+  
+}
+
+window.addEventListener("load", renderDate)
+
+
+
